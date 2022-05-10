4cb184ca09e2be635502ed1805e53d8112ed8baa
AndersonGenez <58364142+AndersonGenez@users.noreply.github.com>
2022-05-09 22:30:47 -0300

    9

--- FuncionesEjercicio9.py
+++ FuncionesEjercicio9.py
@@ -16,6 +16,7 @@ pero en su lugar imprime 5. ¿Qué hay que corregir?
 # 9- Escribir una función que, dado un número de DNI, retorne True si el número es válido y False
 # si no lo es. Para que un número de DNI sea válido debe tener entre 7 y 8 dígito
 
+entrada = int(input('Ingrese un DNI'))
 def validarDNI (num) :
   res = True
   if len(str(num)) < 7 or len(str(num)) > 8 :
@@ -23,4 +24,4 @@ def validarDNI (num) :
 
   return res
 
-print(validarDNI(0))
+print(validarDNI(entrada))
