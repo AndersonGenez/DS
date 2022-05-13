0cf06d6b21cac96fad9757f355ac9b8f7c3bbd73
AndersonGenez <58364142+AndersonGenez@users.noreply.github.com>
2022-05-13 18:25:05 -0300

    Create FuncionesEjercicio4.py

---
+++ FuncionesEjercicio4.py
@@ -0,0 +1,10 @@
+'''
+¿Qué sucede si los nombres de los
+parámetros dentro de la función se
+cambian y se utilizan los nombre a y b en
+lugar de x e y
+'''
+
+'''
+respuesta: No se suceden cambios en el funcionamiento del código ya que los valores son relativos a la posición que ocupan en los parametros de la función.
+'''
