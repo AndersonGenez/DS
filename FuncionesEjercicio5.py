db820da23033a02f1ea6135b38796a3039f842ab
AndersonGenez <58364142+AndersonGenez@users.noreply.github.com>
2022-05-13 18:18:31 -0300

    Create FuncionesEjercicio5.py

---
+++ FuncionesEjercicio5.py
@@ -0,0 +1,12 @@
+'''
+5- Desarrollar un programa que permita ingresar el lado de un cuadrado. Luego preguntar si
+quiere calcular y mostrar su perímetro o su superficie y en función de eso llame a la función
+perímetro o superficie.
+'''
+
+def perimetro(lado) :
+    return lado * 4
+
+def superficie(lado) :
+    return lado * lado
+
